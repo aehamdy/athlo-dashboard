09eb949171bb8ed9cadd6b95f7cb17f8e8b0f148
AhmedEssam <ae.devverse@gmail.com>
2025-12-27 23:28:47 +0200

    Feat: Create Login, Overview and Products pages

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import LoginCard from "@/components/LoginCard";
+
+function Login() {
+  return (
+    <section className="flex justify-center items-center w-full h-dvh bg-light-muted">
+      <LoginCard />
+    </section>
+  );
+}
+
+export default Login;
