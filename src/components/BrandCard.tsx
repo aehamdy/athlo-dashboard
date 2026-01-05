0677c6050030d186c189e02927eaf695f3c8bd1e
AhmedEssam <ae.devverse@gmail.com>
2026-01-05 21:26:04 +0200

    Feat: Create BrandCard component

---
+++ src/components/BrandCard.tsx
@@ -0,0 +1,18 @@
+import Heading from "./sharedComponents/Heading";
+
+export type Brand = {
+  id: number;
+  name: string;
+};
+
+function BrandCard({ brand }: { brand: Brand }) {
+  return (
+    <article className="flex justify-center items-center h-25 bg-accent/30 overflow-hidden">
+      <Heading as="h3" className="text-base font-semibold">
+        {brand.name}
+      </Heading>
+    </article>
+  );
+}
+
+export default BrandCard;
