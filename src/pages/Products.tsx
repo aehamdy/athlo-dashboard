9def653d702f4b527c3b4bde84276bb2ec9f70ed
AhmedEssam <ae.devverse@gmail.com>
2026-01-09 20:42:00 +0200

    Feat: Fetch all products using 'useFetchAll'hook, handle returns whether loading state, error or products related content

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,42 @@
+import { API_ENDPOINTS } from "@/api/endPoints";
+import DashboardSection from "@/components/sharedComponents/DashboardSection";
+import Error from "@/components/sharedComponents/Error";
+import List from "@/components/sharedComponents/List";
+import ListItem from "@/components/sharedComponents/ListItem";
+import Loading from "@/components/sharedComponents/Loading";
+import useFetchAll from "@/hooks/useFetchAll";
+import type { Product } from "@/types";
+
 function Products() {
-  return <div>Products</div>;
+  const { data, error, loading } = useFetchAll<Product[]>(
+    API_ENDPOINTS.products.getAll
+  );
+
+  if (loading) return <Loading size="xl" />;
+
+  if (error) {
+    const message = error instanceof Error ? error.message : String(error);
+    return <Error title="Products" message={message} />;
+  }
+
+  return (
+    <DashboardSection<{ nameEn: string; nameAr: string }>
+      title="Products"
+      buttonLabel="Add Product"
+      description="Add new products to your collection"
+      initialFormValue={{ nameEn: "", nameAr: "" }}
+      onSubmit={() => {}}
+      formComponent={(props) => <div>{JSON.stringify(props)}</div>}
+    >
+      <List>
+        {data?.map((product) => (
+          <ListItem key={product.id}>
+            <div className="">{product.name}</div>
+          </ListItem>
+        ))}
+      </List>
+    </DashboardSection>
+  );
 }
 
 export default Products;
