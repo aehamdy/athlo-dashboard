5eaec9e7331391890f5e6b8031f695d15dd4a1b8
AhmedEssam <ae.devverse@gmail.com>
2026-02-05 23:46:40 +0200

    Feat: Create tableColumns data file for table heads

---
+++ src/config/tableColumns.ts
@@ -0,0 +1,39 @@
+export const PRODUCT_TABLE_COLUMNS = [
+  "Product",
+  "Category",
+  "Brand",
+  "Season",
+  "Club",
+  "Price",
+  "Discount",
+  "Actions",
+];
+
+export const ORDER_TABLE_COLUMNS = [
+  "Order",
+  "Date",
+  "Customer",
+  "Payment",
+  "Total",
+  "Status",
+  "Actions",
+];
+
+export const COUPON_TABLE_COLUMNS = [
+  "Code",
+  "Name",
+  "Percentage",
+  "Starts At",
+  "Expires At",
+  "Type",
+  "Actions",
+];
+
+export const USER_TABLE_COLUMNS = [
+  "No.",
+  "User",
+  "Email",
+  "Phone",
+  "City",
+  "Country",
+];
