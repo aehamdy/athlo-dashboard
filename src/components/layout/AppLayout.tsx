a06fa3f2a8f74b96e00df3c0475a61bb4d8e0d86
AhmedEssam <ae.devverse@gmail.com>
2026-02-11 21:04:36 +0200

    Style: Replace solid black background color into a gradient

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import MainContent from "./MainContent";
 
 function AppLayout() {
   return (
-    <section className="h-screen grid grid-cols-12 p-tiny md:p-regular lg:py-sm lg:pe-sm text-light bg-dark-muted overflow-hidden">
+    <section className="h-screen grid grid-cols-12 p-tiny md:p-regular lg:py-sm lg:pe-sm text-light bg-linear-to-tl from-black to-[#074120] overflow-hidden">
       {/* Sidebar on desktop */}
       <div className="hidden lg:block lg:col-span-2">
         <Sidebar />
