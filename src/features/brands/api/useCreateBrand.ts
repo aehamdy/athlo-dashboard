2fd58782ea42ed647bd1f0bf4c1d8b0972b8ceab
AhmedEssam <ae.devverse@gmail.com>
2026-02-13 23:17:29 +0200

    Feat: Add useCreateBrand hook to handle creating new brands with query invalidation

---
+++ src/features/brands/api/useCreateBrand.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import http from "@/api/http";
+import { API_ENDPOINTS } from "@/api/endPoints";
+import type { Brand } from "@/types";
+
+export const useCreateBrand = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: Brand) => {
+      const res = await http.post(API_ENDPOINTS.brands.create, data);
+      return res.data;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["brands"] });
+    },
+  });
+};
