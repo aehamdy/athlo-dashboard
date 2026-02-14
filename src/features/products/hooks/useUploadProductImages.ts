27e3344c5b08be324ab332fa31af355781ec0a1a
AhmedEssam <ae.devverse@gmail.com>
2026-02-14 23:31:49 +0200

    Feat: Create useUploadProductImages hook

---
+++ src/features/products/hooks/useUploadProductImages.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { ProductImagesFormType } from "../schemas";
+import http from "@/api/http";
+import { API_ENDPOINTS } from "@/api/endPoints";
+
+type UploadPayload = {
+  productId: number;
+  data: ProductImagesFormType;
+};
+
+export function useUploadProductImages() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["upload-product-images"],
+
+    mutationFn: async ({ productId, data }: UploadPayload) => {
+      const formData = new FormData();
+
+      formData.append("productId", productId.toString());
+
+      data.images.forEach((img) => {
+        formData.append("Images", img);
+      });
+
+      await http.post(API_ENDPOINTS.products.addImages, formData);
+    },
+
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["product", variables.productId],
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ["products"],
+      });
+    },
+  });
+}
