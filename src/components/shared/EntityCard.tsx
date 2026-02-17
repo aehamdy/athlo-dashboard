dd19928650c836f0c59c9c968a6cb6d24e36ae25
AhmedEssam <ae.devverse@gmail.com>
2026-02-17 23:24:06 +0200

    Feat: Create EntityCard that returns a card component for category and brand cards

---
+++ src/components/shared/EntityCard.tsx
@@ -0,0 +1,57 @@
+import type { Category } from "@/features/categories/types";
+import { Button } from "../ui/button";
+import AppImage from "./AppImage";
+import Heading from "./Heading";
+import Icon from "./Icon";
+
+type Props = {
+  entity: Category;
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
+};
+
+function EntityCard({ entity, onEdit, onDelete }: Props) {
+  const { id, imageUrl, name } = entity;
+
+  return (
+    <article className="group h-50 lg:h-90 rounded-xl overflow-hidden">
+      <div className="relative w-full h-full">
+        <AppImage
+          src={imageUrl}
+          alt={`${name} image`}
+          className="w-full h-full object-cover object-center"
+        />
+
+        <div className="absolute w-full h-full start-0 top-0 bottom-0 p-compact bg-black/50">
+          <div className="absolute w-full bottom-0 start-0 end-0 p-base text-light lg:group-hover:translate-y-[-1rem] lg:transition-transform lg:duration-normal">
+            <Heading as="h3" className="font-semibold text-lg text-light">
+              {name}
+            </Heading>
+
+            <div className="flex justify-between items-center gap-2 mt-2 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-normal">
+              <Button
+                variant="ghost"
+                onClick={() => onEdit(id)}
+                className="flex-1"
+              >
+                <Icon name="SquarePen" />
+                Edit
+              </Button>
+
+              <Button
+                variant="destructive"
+                className="group/button flex-1"
+                onClick={() => onDelete(id)}
+              >
+                <Icon name="Trash2" className="text-light" />
+                Delete
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default EntityCard;
