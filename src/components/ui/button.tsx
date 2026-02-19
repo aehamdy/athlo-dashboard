fa286f86fd8c1f8f34505dab2c425c7b1c37a07c
AhmedEssam <ae.devverse@gmail.com>
2026-02-19 02:05:26 +0200

    Styles: Replace default variants with my variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "cursor-pointer duration-normal inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        default:
+          "text-dark bg-accent hover:bg-accent-soft hover:shadow-lg active:shadow-none",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "text-accent hover:text-dark bg-light hover:bg-accent border border-accent",
+        outline: "text-dark bg-light hover:bg-accent border border-accent",
+        destructive:
+          "text-light bg-red-500/80 hover:bg-red-500 active:bg-red-500 ",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "text-dark lg:text-light hover:text-dark active:text-dark bg-light/80 lg:bg-light/40 lg:hover:bg-light/90 active:bg-light/90 border-light/20 ",
+        // default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        // destructive:
+        //   "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        // outline:
+        //   "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        // secondary:
+        //   "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        // ghost:
+        //   "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
         plain: "cursor-pointer",
         icon: "size-7 cursor-pointer",
