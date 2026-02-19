9df4f1a9d72eed02335849bec725a0bd6680b5e0
AhmedEssam <ae.devverse@gmail.com>
2026-02-19 02:33:26 +0200

    Refactor: Move Header to Overview page

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,5 +1,13 @@
+import Header from "@/components/shared/Header";
+
 function Overview() {
-  return <div>Overview</div>;
+  return (
+    <div className="flex flex-col gap-sm md:gap-base lg:gap-sm h-full">
+      <Header />
+
+      <div className="bg-white">Overview</div>
+    </div>
+  );
 }
 
 export default Overview;
