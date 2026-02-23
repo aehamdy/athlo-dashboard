ebbe9cbe52cde936b3f8459123bb84443f9e6c2a
AhmedEssam <ae.devverse@gmail.com>
2026-02-23 22:39:00 +0200

    Feat: Add useProduct hook for fetching product details

---
+++ src/features/products/hooks/useProduct.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import http from "@/api/http";
+import { API_ENDPOINTS } from "@/api/endPoints";
+import type { Product } from "@/types";
+
+export const useProduct = (id: number | null) => {
+  const { data } = useQuery({
+    queryKey: ["product", id],
+    queryFn: async () => {
+      if (!id) return null;
+      const response = await http.get(
+        API_ENDPOINTS.products.getById(id.toString()),
+      );
+      return response.data.data as Product;
+    },
+    enabled: !!id,
+  });
+
+  return { basePrice: data?.basePrice ?? 0 };
+};
