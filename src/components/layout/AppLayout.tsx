d60d6f7619136315eee99af66b60bf0404ca28cc
AhmedEssam <ae.devverse@gmail.com>
2026-02-23 23:37:40 +0200

    Refactor: Ensure full-height AppLayout and fix global scrolling

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,11 +5,11 @@ function AppLayout() {
   return (
     <section className="h-screen grid grid-cols-12 p-tiny md:p-regular lg:py-sm lg:pe-sm text-light bg-linear-to-tl from-black to-[#074120] overflow-hidden">
       {/* Sidebar on desktop */}
-      <div className="hidden lg:block lg:col-span-2">
+      <div className="hidden lg:block lg:col-span-2 h-full">
         <Sidebar />
       </div>
 
-      <div className="col-span-12 lg:col-span-10 h-full">
+      <div className="col-span-12 lg:col-span-10 h-full min-h-0">
         <MainContent />
       </div>
     </section>
