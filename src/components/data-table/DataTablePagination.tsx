2a9d777c36e278a909bed8a72eb307879bc5d47d
AhmedEssam <ae.devverse@gmail.com>
2026-02-24 22:59:56 +0200

    Styles: Adjust styles on different screens

--- src/components/data-table/DataTablePagination.tsx
+++ src/components/data-table/DataTablePagination.tsx
@@ -1,3 +1,4 @@
+import Icon from "../shared/Icon";
 import { Button } from "../ui/button";
 import type { Table, PaginationState } from "@tanstack/react-table";
 
@@ -13,23 +14,31 @@ function DataTablePagination<TData = unknown>({
   pageCount,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex justify-between py-sm px-md">
+    <div className="flex justify-between items-center gap-regular py-sm px-md">
       <Button
         onClick={() => table.previousPage()}
         disabled={!table.getCanPreviousPage()}
       >
-        Previous
+        <Icon name="ArrowLeft" />
+        <span className="hidden md:flex">Previous</span>
       </Button>
 
-      <span className="px-base py-sm">
-        Page {pagination.pageIndex + 1} of {pageCount}
+      <span className="text-sm font-medium">
+        <span className="md:hidden">
+          {pagination.pageIndex + 1} / {pageCount}
+        </span>
+
+        <span className="hidden md:inline">
+          Page {pagination.pageIndex + 1} of {pageCount}
+        </span>
       </span>
 
       <Button
         onClick={() => table.nextPage()}
         disabled={!table.getCanNextPage()}
       >
-        Next
+        <span className="hidden md:flex">Next</span>
+        <Icon name="ArrowRight" />
       </Button>
     </div>
   );
