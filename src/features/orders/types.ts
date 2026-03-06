ecca29027cd0e26f4c47f256ece971fa1b7185a1
AhmedEssam <ae.devverse@gmail.com>
2026-03-06 17:44:57 +0200

    Feat: Create orders types file

---
+++ src/features/orders/types.ts
@@ -0,0 +1,9 @@
+export type Order = {
+  orderId: number;
+  userId: number;
+  userEmail: string;
+  totalAmount: number;
+  orderStatus: string;
+  paymentStatus: string;
+  createdAt: string;
+};
