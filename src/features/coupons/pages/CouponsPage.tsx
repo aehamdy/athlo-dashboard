a1f2634cba1ea42b840b0df84cc068a62d24f4ca
AhmedEssam <ae.devverse@gmail.com>
2026-03-07 22:52:18 +0200

    Feat: Add coupon delete functionality with invalidation and toast on success and showign toast on error

--- src/features/coupons/pages/CouponsPage.tsx
+++ src/features/coupons/pages/CouponsPage.tsx
@@ -1,23 +1,54 @@
-import AddCouponForm from "@/features/coupons/components/AddCouponForm";
-import DashboardSection from "@/components/shared/DashboardSection";
-import List from "@/components/shared/List";
-import TableWrapper from "@/components/shared/TableWrapper";
-import CouponsTable from "@/features/coupons/components/CouponsTable";
+import { DataTable } from "@/components/data-table/DataTable";
+import DashboardPageLayout from "@/components/shared/DashboardPageLayout";
+import couponsColumns from "../columns";
+import useFetchAllCoupons from "../hooks/useFetchAllCoupons";
+import type { Coupon } from "../types";
+import { useState } from "react";
+import ConfirmDeleteModal from "@/components/shared/ConfirmDeleteModal";
+import useDeleteCoupon from "../hooks/useDeleteCoupon";
 
 function Coupons() {
+  const [couponToDelete, setCouponToDelete] = useState<Coupon | null>(null);
+  const { data: coupons, isLoading, isError } = useFetchAllCoupons();
+  const deleteCoupon = useDeleteCoupon();
+
+  const columns = couponsColumns((id) => {
+    const coupon = coupons?.find((coupon) => coupon.id === id);
+    if (coupon) setCouponToDelete(coupon);
+  });
+
+  const handleConfirmDelete = () => {
+    if (!couponToDelete) return;
+
+    deleteCoupon.mutate(couponToDelete.id, {
+      onSuccess: () => setCouponToDelete(null),
+    });
+  };
+
   return (
-    <DashboardSection
+    <DashboardPageLayout
       title="Coupons"
-      buttonLabel="Add Coupon"
       description="Create, manage, and track discount coupons for your store."
-      formComponent={<AddCouponForm />}
     >
-      <List variant="table">
-        <TableWrapper>
-          <CouponsTable />
-        </TableWrapper>
-      </List>
-    </DashboardSection>
+      <div className="flex flex-col gap-base h-full">
+        <DataTable
+          data={coupons ?? []}
+          isLoading={isLoading}
+          error={isError}
+          columns={columns}
+        />
+      </div>
+
+      {couponToDelete && (
+        <ConfirmDeleteModal
+          item={couponToDelete}
+          setItem={setCouponToDelete}
+          itemLabel="coupon"
+          getDisplayName={(coupon) => coupon.code}
+          onConfirm={handleConfirmDelete}
+        />
+      )}
+    </DashboardPageLayout>
   );
 }
 
