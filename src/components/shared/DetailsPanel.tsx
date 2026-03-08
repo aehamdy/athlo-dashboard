5a737e83fa7ff71084270c63f1ae3dc35b9c9739
AhmedEssam <ae.devverse@gmail.com>
2026-03-08 06:45:59 +0200

    Feat: Display coupon details upon click on the coupon

--- src/components/shared/DetailsPanel.tsx
+++ src/components/shared/DetailsPanel.tsx
@@ -1,19 +1,49 @@
-import { Sheet, SheetContent } from "../ui/sheet";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "../ui/sheet";
 
 type DetailsPanelProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  width?: string;
   children?: React.ReactNode;
 };
 
-function DetailsPanel({ open, onOpenChange, children }: DetailsPanelProps) {
+function DetailsPanel({
+  open,
+  onOpenChange,
+  title,
+  description,
+  width,
+  children,
+}: DetailsPanelProps) {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent
         side="right"
-        className="min-w-[95%] md:min-w-3/4 lg:min-w-1/2 flex flex-col gap-0 overflow-y-auto"
+        className={`${width || "min-w-[95%] md:min-w-3/4 lg:min-w-1/2"} flex flex-col gap-0 overflow-y-auto`}
       >
+        <SheetHeader className="gap-tiny p-regular border-b">
+          <SheetTitle>{title}</SheetTitle>
+          <SheetDescription>{description}</SheetDescription>
+        </SheetHeader>
+
         {children}
+
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
