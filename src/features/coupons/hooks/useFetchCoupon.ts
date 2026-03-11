e06c19016b993a2c92c36d197e9c38810a88b1db
AhmedEssam <ae.devverse@gmail.com>
2026-03-11 02:31:47 +0200

    Refactor: Update useFetchCoupon argument type and add CouponStatus and CouponFormMode types

--- src/features/coupons/hooks/useFetchCoupon.ts
+++ src/features/coupons/hooks/useFetchCoupon.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { couponsService } from "../services/couponsService";
 import { couponKeys } from "./couponKeys";
 
-function useFetchCoupon(couponId: number) {
+function useFetchCoupon(couponId: number | null) {
   return useQuery({
-    queryKey: couponKeys.detail(couponId),
-    queryFn: () => couponsService.getById(couponId),
+    queryKey: couponKeys.detail(couponId!),
+    queryFn: () => couponsService.getById(couponId!),
     enabled: !!couponId,
   });
 }
