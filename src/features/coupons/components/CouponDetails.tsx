94dc166bd7d3ab17e25d9765b5db2806cc64291b
AhmedEssam <ae.devverse@gmail.com>
2026-03-12 02:48:03 +0200

    Refactor: Implement Accordion component from Shadcn

--- src/features/coupons/components/CouponDetails.tsx
+++ src/features/coupons/components/CouponDetails.tsx
@@ -1,15 +1,24 @@
 import Heading from "@/components/shared/Heading";
 import type { Coupon } from "../types";
-import { formatDateTime } from "@/utils/formatDateTime ";
 import getCouponStatus from "../utils/getCouponStatus";
 import { Badge } from "@/components/ui/badge";
 import getCouponStatusStyles from "../utils/getCouponStatusStyles";
+import CouponApplicableProducts from "./CouponApplicableProducts";
+import Icon from "@/components/shared/Icon";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { formatDateTime } from "@/utils/formatDateTime";
 
 type CouponDetailsProps = {
   coupon: Coupon;
+  setIsPickerOpen: (open: boolean) => void;
 };
 
-function CouponDetails({ coupon }: CouponDetailsProps) {
+function CouponDetails({ coupon, setIsPickerOpen }: CouponDetailsProps) {
   const { date: startDate } = formatDateTime(coupon.startDate);
   const { date: endDate } = formatDateTime(coupon.endDate);
   const couponStatus = getCouponStatus({
@@ -27,8 +36,17 @@ function CouponDetails({ coupon }: CouponDetailsProps) {
           <span className="absolute top-1/2 -translate-y-1/2 left-0 w-full h-0.5 border-b border-dashed border-accent-strong" />
 
           <div className="flex flex-col justify-start gap-tiny w-full">
-            {/* <Icon name="TicketPercent" className="text-white" size={24} /> */}
-            <p className="font-semibold text-md text-dark">{coupon.code}</p>
+            <div className="flex items-center gap-sm">
+              <Icon name="TicketPercent" className="text-gray-700" size={18} />
+
+              <Heading
+                as="h3"
+                className="font-semibold text-sm md:text-sm text-dark"
+              >
+                {coupon.code}
+              </Heading>
+            </div>
+
             <p className="font-semibold text-xs text-gray-700">
               {coupon.percentage}% OFF{" "}
               {coupon.type === "Global"
@@ -49,63 +67,78 @@ function CouponDetails({ coupon }: CouponDetailsProps) {
       </section>
 
       <section className="h-[310px] overflow-y-auto">
-        <section className="flex flex-col gap-sm py-tiny border-b">
-          <Heading
-            as="h5"
-            className="font-semibold text-tiny md:text-tiny text-gray-400 uppercase tracking-widest"
-          >
-            Configuration
-          </Heading>
-
-          <div className="space-y-xs">
-            <div className="flex justify-between items-center">
-              <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
-                Status
-              </p>
-
-              <Badge
-                variant="default"
-                className={`uppercase rounded-md ${couponStatusClass}`}
+        <Accordion type="single" collapsible defaultValue="configuration">
+          <AccordionItem value="configuration">
+            <AccordionTrigger className="py-compact cursor-pointer">
+              <Heading
+                as="h5"
+                className="font-semibold text-tiny md:text-tiny text-gray-400 uppercase tracking-widest"
               >
-                {couponStatus}
-              </Badge>
-            </div>
+                Configuration
+              </Heading>
+            </AccordionTrigger>
 
-            <div className="flex justify-between items-center">
-              <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
-                Discount Type
-              </p>
-              <p className="font-semibold text-xs text-zinc-800">
-                {coupon.type}
-              </p>
-            </div>
+            <AccordionContent className="space-y-xs pb-tiny px-tiny">
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
+                  Status
+                </p>
 
-            <div className="flex justify-between items-center">
-              <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
-                Discount Value
-              </p>
-              <p className="font-semibold text-xs text-red-600">
-                -{coupon.percentage}%
-              </p>
-            </div>
+                <Badge
+                  variant="default"
+                  className={`uppercase rounded-md ${couponStatusClass}`}
+                >
+                  {couponStatus}
+                </Badge>
+              </div>
 
-            <div className="flex justify-between items-center">
-              <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
-                Start Date
-              </p>
-              <p className="font-semibold text-xs text-zinc-800">{startDate}</p>
-            </div>
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
+                  Discount Type
+                </p>
+                <p className="font-semibold text-xs text-zinc-800">
+                  {coupon.type === "ProductSpecific"
+                    ? "Product Specific"
+                    : coupon.type === "Global"
+                      ? "Global"
+                      : coupon.type}
+                </p>
+              </div>
 
-            <div className="flex justify-between items-center">
-              <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
-                End Date
-              </p>
-              <p className="font-semibold text-xs text-zinc-800">{endDate}</p>
-            </div>
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
+                  Discount Value
+                </p>
+                <p className="font-semibold text-xs text-red-600">
+                  -{coupon.percentage}%
+                </p>
+              </div>
 
-            <div className=""></div>
-          </div>
-        </section>
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
+                  Start Date
+                </p>
+                <p className="font-semibold text-xs text-zinc-800">
+                  {startDate}
+                </p>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-tiny text-gray-500 uppercase tracking-widest">
+                  End Date
+                </p>
+                <p className="font-semibold text-xs text-zinc-800">{endDate}</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        {coupon.type === "ProductSpecific" && coupon.id && (
+          <CouponApplicableProducts
+            couponId={coupon.id}
+            setIsPickerOpen={setIsPickerOpen}
+          />
+        )}
       </section>
     </section>
   );
