6c55b790c83cc3766cfe1a5dfd216847708b26aa
AhmedEssam <ae.devverse@gmail.com>
2026-03-18 19:35:23 +0200

    Feat: Create updateOrderStatus service

--- src/features/orders/services/ordersService.ts
+++ src/features/orders/services/ordersService.ts
@@ -1,5 +1,5 @@
-import { API_ENDPOINTS } from "@/api/endpoints";
-import http from "@/api/http";
+import { API_ENDPOINTS } from '@/api/endpoints';
+import http from '@/api/http';
 
 const ordersService = {
   getAll: async () => {
@@ -7,8 +7,17 @@ const ordersService = {
 
     return response.data.data;
   },
+
   getCouponDetails: async (id: number) => {
     const response = await http.get(API_ENDPOINTS.orders.getById(id));
+    return response.data.data;
+  },
+
+  updateOrderStatus: async (data: { orderId: number; status: number }) => {
+    const response = await http.put(API_ENDPOINTS.orders.updateOrderStatus, {
+      orderId: data.orderId,
+      status: data.status,
+    });
     return response.data.data;
   },
 };
