44a89fb0e378ad791fb8fc97e6c5d91493c9034a
AhmedEssam <ae.devverse@gmail.com>
2026-03-29 00:58:28 +0200

    Refactor: Update category form to be fully controlled by RHF and its schema

--- src/features/categories/category.schema.ts
+++ src/features/categories/category.schema.ts
@@ -1,9 +1,10 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-export const categorySchema = z.object({
-  nameEn: z.string().min(3, "English name is required"),
-  nameAr: z.string().min(3, "Arabic name is required"),
-  image: z.instanceof(File).or(z.undefined()),
-});
-
-export type CategoryFormValues = z.infer<typeof categorySchema>;
+export const categorySchema = (isEditMode: boolean) =>
+  z.object({
+    nameEn: z.string().min(1, 'English name is required'),
+    nameAr: z.string().min(1, 'Arabic name is required'),
+    image: isEditMode
+      ? z.any().optional()
+      : z.instanceof(File).refine((file) => file.size > 0, 'Image is required'),
+  });
