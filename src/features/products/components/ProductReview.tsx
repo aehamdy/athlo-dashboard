1472ad81c8dbe2510063ef892754682f44e9ffff
AhmedEssam <ae.devverse@gmail.com>
2026-03-28 19:33:17 +0200

    Style: Adjust reviews list height and set responsive min-height

--- src/features/products/components/ProductReview.tsx
+++ src/features/products/components/ProductReview.tsx
@@ -1,36 +1,42 @@
-import Heading from "@/components/shared/Heading";
-import type { IProductReview } from "../types"
-import Icon from "@/components/shared/Icon";
-import { formatDateTime } from "@/utils/formatDateTime";
+import Heading from '@/components/shared/Heading';
+import type { IProductReview } from '../types';
+import Icon from '@/components/shared/Icon';
+import { formatDateTime } from '@/utils/formatDateTime';
 
 function ProductReview({ review }: { review: IProductReview }) {
-    const { date } = formatDateTime(review.createdAt);
+  const { date } = formatDateTime(review.createdAt);
 
-    return (
-        <article className="">
-            <div className="flex justify-between">
-                <Heading as="h6" className="text-sm font-semibold">{review.userName}</Heading>
+  return (
+    <article className="overflow-hidden">
+      <div className="flex flex-col gap-sm">
+        <div className="flex justify-between">
+          <Heading as="h6" className="text-sm font-semibold">
+            {review.userName}
+          </Heading>
 
-                <div className="flex items-center gap-xs text-sm">
-                    {Array.from({ length: 5 }).map((_, index) => {
-                        const isFilled = index < review.rating;
+          <div className="flex items-center gap-xs text-sm">
+            {Array.from({ length: 5 }).map((_, index) => {
+              const isFilled = index < review.rating;
 
-                        return (
-                            <Icon
-                                key={index}
-                                name="Star"
-                                className={isFilled ? "fill-accent text-accent" : "text-accent"}
-                            />
-                        )
-                    })}
-                </div>
-            </div>
+              return (
+                <Icon
+                  key={index}
+                  name="Star"
+                  className={
+                    isFilled ? 'fill-accent text-accent' : 'text-accent'
+                  }
+                />
+              );
+            })}
+          </div>
+        </div>
 
-            <p className="text-sm text-neutral-muted">{review.comment}</p>
+        <p className="my-sm text-sm text-neutral-muted">{review.comment}</p>
 
-            <p className="text-end text-xs text-neutral-muted">{date}</p>
-        </article>
-    )
+        <p className="text-end text-xs text-neutral-muted">{date}</p>
+      </div>
+    </article>
+  );
 }
 
-export default ProductReview
+export default ProductReview;
--- src/features/products/components/ProductReviewsList.tsx
+++ src/features/products/components/ProductReviewsList.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 function ProductReviewsList({ children }: { children: ReactNode }) {
   return (
-    <ul className="h-[410px] md:h-[450px] lg:max-h-[350px] py-sm space-y-sm overflow-y-auto">
+    <ul className="min-h-27.5 max-h-77.5 md:min-h-31.25 md:max-h-97.5 py-sm space-y-sm overflow-y-auto">
       {children}
     </ul>
   );
