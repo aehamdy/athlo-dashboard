6c11a0c19bda377b06938f9d0000ec54484f96c3
AhmedEssam <ae.devverse@gmail.com>
2026-05-02 17:48:46 +0300

    Feat: Handle deleting in-store orders

--- src/features/inStoreOrders/pages/InStoreOrdersPage.tsx
+++ src/features/inStoreOrders/pages/InStoreOrdersPage.tsx
@@ -2,15 +2,30 @@ import { DataTable } from '@/components/data-table/DataTable';
 import DashboardPageLayout from '@/components/shared/DashboardPageLayout';
 import useFetchInStoreOrdersList from '../hooks/useFetchInStoreOrdersList';
 import inStoreOrdersColumns from '../columns';
+import { useMemo, useState } from 'react';
+import type { InStoreOrderListItem } from '../types';
+import ConfirmDeleteModal from '@/components/shared/ConfirmDeleteModal';
+import useDeleteInStoreOrder from '../hooks/useDeleteInStoreOrder';
 
 function InStoreOrdersPage() {
+  const [orderToDelete, setOrderToDelete] =
+    useState<InStoreOrderListItem | null>(null);
+
   const {
     data: inStoreOrders,
     isLoading,
     isError,
   } = useFetchInStoreOrdersList();
 
-  const columns = inStoreOrdersColumns();
+  const columns = useMemo(() => inStoreOrdersColumns(setOrderToDelete), []);
+
+  const { ...mutation } = useDeleteInStoreOrder({ setOrderToDelete });
+
+  const confirmDelete = () => {
+    if (orderToDelete) {
+      mutation.mutate(orderToDelete.id);
+    }
+  };
 
   return (
     <DashboardPageLayout title="In-Store Orders">
@@ -22,6 +37,15 @@ function InStoreOrdersPage() {
           columns={columns}
         />
       </div>
+
+      <ConfirmDeleteModal
+        item={orderToDelete}
+        setItem={setOrderToDelete}
+        itemLabel="order"
+        getDisplayName={(o) => o?.saleNumber ?? ''}
+        onConfirm={confirmDelete}
+        isPending={mutation.isPending}
+      />
     </DashboardPageLayout>
   );
 }
