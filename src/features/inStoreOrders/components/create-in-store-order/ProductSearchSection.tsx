d7e4459bd18f9a353668de4b0f35fb8b1e866a90
AhmedEssam <ae.devverse@gmail.com>
2026-05-07 00:05:47 +0300

    Feat: Add back button to navigate to in-store order page

--- src/features/inStoreOrders/components/create-in-store-order/ProductSearchSection.tsx
+++ src/features/inStoreOrders/components/create-in-store-order/ProductSearchSection.tsx
@@ -1,5 +1,8 @@
 import Heading from '@/components/shared/Heading';
+import Icon from '@/components/shared/Icon';
 import { Input } from '@/components/ui/input';
+import { ROUTE_PATHS } from '@/routes/paths';
+import { Link } from 'react-router-dom';
 
 interface Props {
   searchQuery: string;
@@ -16,6 +19,15 @@ function ProductSearchSection({ searchQuery, setSearchQuery }: Props) {
   return (
     <div className="flex flex-col gap-sm">
       <div className="flex flex-col gap-xs">
+        <div className="flex items-center gap-sm w-fit">
+          <Link
+            to={ROUTE_PATHS.dashboard.orders.inStore}
+            className="flex items-center p-sm hover:text-black hover:bg-accent-soft border border-gray-300 hover:border-transparent rounded-md transition-colors duration-normal"
+          >
+            <Icon name="ArrowLeft" className="text-current" />
+          </Link>
+        </div>
+
         <Heading as="h4">Add items</Heading>
 
         <p className="">Search to find and add items.</p>
